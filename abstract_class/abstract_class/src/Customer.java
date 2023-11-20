bcf23fd432e814dd70dc62da05e4263e17cfcf05
ahmadaalifi <148086802+ahmadaalifi@users.noreply.github.com>
2023-11-20 12:36:29 +0700

    Add files via upload

---
+++ abstract_class/abstract_class/src/Customer.java
@@ -0,0 +1,22 @@
+public class Customer extends baseCarwash {
+
+    @Override
+    public void Paket1() {
+        System.out.println("Salju dan Vacum");
+    }
+
+    @Override
+    public void Paket2() {
+        System.out.println("Salju saja");
+    }
+
+    @Override
+    public void Paket3() {
+        System.out.println("Vacum saja");
+    }
+
+    @Override
+    public void jadwalShift() {
+    }
+    
+}
---
+++ abstract_class/abstract_class/src/baseCarwash.java
@@ -0,0 +1,15 @@
+public abstract class baseCarwash {
+    public String NamaCust;
+    public String Harga;
+    public String Member;
+    public String caraBayar;
+    public String jenisMobil;
+
+public abstract void Paket1(); {}
+public abstract void Paket2(); {}
+public abstract void Paket3(); {}
+
+public abstract void jadwalShift(); {
+    
+}
+}
