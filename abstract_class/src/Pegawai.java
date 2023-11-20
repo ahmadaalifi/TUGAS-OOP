48c640892a0fe0ce36d1588a5446227eb7ab4229
ahmadaalifi <148086802+ahmadaalifi@users.noreply.github.com>
2023-11-20 12:37:40 +0700

    Add files via upload

---
+++ abstract_class/src/Pegawai.java
@@ -0,0 +1,30 @@
+public class Pegawai extends baseCarwash {
+    public String namaPegawai;
+    public String shiftPegawai;
+
+
+    @Override
+    public void Paket1() {
+        
+    }
+
+    @Override
+    public void Paket2() {
+        
+    }
+
+    @Override
+    public void Paket3() {
+
+    }
+
+    @Override
+    public void jadwalShift() {
+        System.out.println("Shit 1 07:00-12:00");
+    }
+    public void jadwalShift(String Sh2) {
+        System.out.println("Shift 3 12:00-18:00");
+    }
+    
+    
+}
